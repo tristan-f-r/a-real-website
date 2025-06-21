ace2611bc3572d10b294bc9708ff0c0cc4ffe862
Tristan F. <LeoDog896@hotmail.com>
2025-06-20 23:59:16 -0700

    chore: init

---
+++ src/lib/EditableComponent.svelte
@@ -0,0 +1,8 @@
+<script>
+    import { draggable } from '@neodrag/svelte';
+</script>
+  
+<div use:draggable>
+    <h1>make a real website</h1>
+    <p>real website creation. for the complement of the rest of us.</p>
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+	import EditableComponent from "$lib/EditableComponent.svelte";
+</script>
+
+<EditableComponent />
