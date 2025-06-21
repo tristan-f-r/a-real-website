2088cbbc51efead4e64ee4d2986044c62a4ef634
Tristan F. <LeoDog896@hotmail.com>
2025-06-21 01:06:02 -0700

    feat: test page demo

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,17 +2,21 @@
 	import ConsoleHook from "$lib/ConsoleHook.svelte";
     import EditableComponent, { type ComponentData } from "$lib/EditableComponent.svelte";
 	import Toolbar from "$lib/Toolbar.svelte";
+    import testPage from './testPage.json?raw'
 
     const defaultComponent = Object.freeze({
         content: "<h1>my component</h1>",
         position: { x: 0, y: 0 }
     });
 
-    let components: ComponentData[] = $state([structuredClone(defaultComponent)])
+    let components: ComponentData[] = $state(JSON.parse(testPage))
 </script>
 
-<Toolbar bind:components={components} {defaultComponent} />
 <ConsoleHook bind:components={components} />
+
+<header>
+    <Toolbar bind:components={components} {defaultComponent} />
+</header>
 
 <main>
     {#each components as _, i}
